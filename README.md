77f5cddde21be603b5518cf3e4a705833adffc52
Phil Marshall <dr.phil.marshall@gmail.com>
2012-05-16 23:56:18 -0400

    First working version

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
 colorjpg
 ========
 
-Color composite image creation following the Lupton et al (2004) algorithm.
+Color composite image creation from sets of input FITS files following the
+Lupton et al (2004) algorithm.
+
+Author: Phil Marshall <dr.phil.marshall@gmail.com> 
+Date: 11 May, 2012
+
+Based on: Patrick Jonsson's make_color.py, the jpg creation routines in
+IDLutils, and most of all Lupton et al 2004.
+
---
+++ colorjpg/io.py
@@ -0,0 +1,122 @@
+# ======================================================================
+
+''' 
+Functions for reading and writing sets of fits files, including header
+information.
+'''
+
+# ======================================================================
+# Globally useful modules:
+
+import numpy,pyfits,Image
+
+vb = 0
+
+# ======================================================================
+
+class channel:
+
+   def __init__(self,fitsfile):
+   
+      # Read in image and header:
+      hdulist = pyfits.open(fitsfile)
+      self.image = hdulist[0].data
+      self.hdr = hdulist[0].header
+      hdulist.close()
+
+      return
+      
+   def set_scale(self,manually=False):
+   
+      if manually:
+        self.scale = manually
+        
+      else:  
+        # Get zero point and exptime:
+        self.zpt = extract_zeropoint(self.hdr)
+        self.exptime = self.hdr['EXPTIME']
+
+        # Extract filter and infer wavelength, telescope:
+        self.filter = self.hdr['filter']
+        self.wavelength = filter2wavelength(self.filter)
+
+        # Suggest scale for this image based on exptime and zpt:
+        self.scale = numpy.sqrt(self.exptime)*10**(self.zpt + 2.5*numpy.log10(self.wavelength) - 26.0)
+
+      return
+   
+   def apply_scale(self):
+   
+      self.image *= self.scale
+      
+      return
+      
+# ======================================================================
+
+def extract_zeropoint(hdr):
+
+   # Where did the data come from?
+   origin = hdr['ORIGIN']
+
+   # Look for zero points:
+   if origin == 'CFHT':
+     zpt = hdr['PHOT_C']
+   
+   else:
+     raise "Unrecognised origin: "+origin
+   
+   return zpt
+   
+# ----------------------------------------------------------------------
+   
+def filter2wavelength(fname):
+
+   # CFHT MegaCam (from http://www.cfht.hawaii.edu/Instruments/Imaging/Megacam/specsinformation.html)
+   if fname == 'u.MP9301':
+     L = 3740
+   elif fname == 'g.MP9401':
+     L = 4870
+   elif fname == 'r.MP9601':
+     L = 6250
+   elif fname == 'i.MP9701':
+     L = 7700
+   elif fname == 'z.MP9801':
+     L = 9000
+
+   # SDSS:
+   
+   # DES:
+   
+   # etc
+   return L
+
+# ----------------------------------------------------------------------
+   
+def check_image_shapes(r,g,b):
+
+   if (numpy.shape(r) != numpy.shape(g)) or \
+      (numpy.shape(r) != numpy.shape(b)):
+      raise "Image arrays are of different shapes, exiting"
+
+   return
+  
+# ----------------------------------------------------------------------
+# Make an 8 bit integer image cube from three channels:
+   
+def pack_up(r,g,b):
+
+   NX,NY = numpy.shape(r)
+
+   x = numpy.zeros([NX,NY,3])
+   x[:,:,0] = r
+   x[:,:,1] = g
+   x[:,:,2] = b
+
+   x = numpy.clip(x,0.0,1.0)
+   x = x*255
+
+   return Image.fromarray(x.astype(numpy.uint8))
+
+# ======================================================================
+
+
